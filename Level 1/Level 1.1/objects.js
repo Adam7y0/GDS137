f20bda1399a91b67df5ca9811182d466b98d91f7
Adam7y0 <acodonnell@email.neit.edu>
2025-04-23 15:47:29 -0400

    collisions

--- Level 1/Level 1.1/objects.js
+++ Level 1/Level 1.1/objects.js
@@ -10,36 +10,65 @@ context = canvas.getContext("2d");
 
 timer = setInterval(animate, interval);
 
-function GameObject(x,y,w,h,color)
-{
-	this.x = x
-	this.y = y
-	this.w = w
-	this.h = h
-	this.color = color
+// function GameObject(x,y,w,h,color)
+// {
+// 	this.x = x
+// 	this.y = y
+// 	this.w = w
+// 	this.h = h
+// 	this.color = color
 
-this.drawCircle = function()
-	{
-		context.save();
-			context.fillStyle = this.color;
-			context.beginPath();
-			context.translate(this.x, this.y);
-			context.arc(500, 0, this.w/2, 0, 360 *Math.PI/180, true);
-			context.closePath();
-			context.fill();
-		context.restore();
+// this.drawCircle = function()
+// 	{
+// 		context.save();
+// 			context.fillStyle = this.color;
+// 			context.beginPath();
+// 			context.translate(this.x, this.y);
+// 			context.arc(500, 0, this.w/2, 0, 360 *Math.PI/180, true);
+// 			context.closePath();
+// 			context.fill();
+// 		context.restore();
 		
-	}
-}
+// 	}
+// }
 
-var ball = new GameObject(30, canvas.height/2,50,10,"red");
+var ball = new GameObject(30, canvas.height/2,50,50,"red");
+
+ball.vx = -1
+ball.vy = -1
+
+
 
 function animate()
 {
 	context.clearRect(0,0,canvas.width, canvas.height);	
 
 	ball.drawCircle();
-	console.log(ball.w)
-	ball.x += 2;
+	console.log(ball.x)
+	ball.move()
+
+	//right side of canvas
+	if(ball.x + ball.width/2 > canvas.width)
+	{
+		ball.vx *= -1
+	}
+
+	//left side of canvas
+	if(ball.x - ball.width/2 < 0){
+		ball.vx *= -1
+
+	}
+
+	//bottom of canvas
+	if(ball.y + ball.height/2 > canvas.height){
+		ball.vy *= -1
+	}
+
+	//top of canvas
+	if(ball.y - ball.height/2 < 0)
+	{
+		ball.vy *= -1
+	}
+
 	
 }
